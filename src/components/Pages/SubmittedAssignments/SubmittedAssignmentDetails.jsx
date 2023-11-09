0538267b0b1d9862ec5db1c824ee82e742fd4c12
almuktadiir <muktadir.j729.com>
2023-11-09 18:11:41 +0600

    react pdf added

--- src/components/Pages/SubmittedAssignments/SubmittedAssignmentDetails.jsx
+++ src/components/Pages/SubmittedAssignments/SubmittedAssignmentDetails.jsx
@@ -1,23 +1,54 @@
 
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-// import SubmitMarks from './SubmitMarks';
+import { Document, Page } from 'react-pdf';
+
+import { pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.js',
+    import.meta.url,
+).toString();
+
+
+
 
 
 
 const SubmittedAssignmentDetails = ({ sData }) => {
-    const {_id, title, marks, examineeName, } = sData;
+    const { _id, title, marks, examineeName, pdf } = sData;
+    console.log(pdf);
+    // const pdfUrl = 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf'
 
     return (
         <div className="card card-side bg-base-100 shadow-xl">
-            {/* <figure><img src={image} alt="Movie" /></figure> */}
             <div className="card-body">
                 <h2 className="card-title">Assignment: {title}</h2>
                 <p>Total Marks: {marks}</p>
                 <p>Examinee Name: {examineeName}</p>
+
+
+                <div className='flex justify-center'>
+
+                
+                <div>
+                    {/* You can open the modal using document.getElementById('ID').showModal() method */}
+                    <button className="btn bg-green-300 w-full" onClick={() => document.getElementById('my_modal_3').showModal()}>View PDF</button>
+                    <dialog id="my_modal_3" className="modal">
+                        <div className="modal-box">
+                            <form method="dialog">
+                                {/* if there is a button in form, it will close the modal */}
+                                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                            </form>
+                            <Document file={pdf}>
+                                <Page pageNumber={1}></Page>
+                            </Document>
+                        </div>
+                    </dialog>
+                </div>
                 <Link to={`/submitAssignMarking/${_id}`}><button className='btn bg-green-300 w-full'>Give Mark</button>
-                {/* <SubmitMarks sData={sData}></SubmitMarks> */}
                 </Link>
+                </div>
             </div>
         </div>
     );
