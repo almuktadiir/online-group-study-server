98f8e569e1352249fe3eec7e8f31dbf42fb608aa
almuktadiir <muktadir.j729.com>
2023-11-09 12:27:50 +0600

    conditional function added

--- src/components/Pages/MyAssignments/MyAssignments.jsx
+++ src/components/Pages/MyAssignments/MyAssignments.jsx
@@ -2,23 +2,35 @@ import { useContext, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 import MyAssignmentCard from "./MyAssignmentCard";
+import img from '../../../assets/no_data.jpg'
 
 
 const MyAssignments = () => {
     const myAssignData = useLoaderData();
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
     const filterData = myAssignData.filter(myData => myData.submittedUserEmail === user?.email);
     const [currentUserData, setCurrentUserData] = useState(filterData);
 
     return (
-        <div className="max-w-7xl mx-auto my-14 grid grid-cols-3 gap-6">
+        <div>
             {
-                currentUserData.map(currentUsers => <MyAssignmentCard
-                key={currentUsers._id}
-                currentUsers={currentUsers}
-                currentUserData={currentUserData}
-                setCurrentUserData={setCurrentUserData}
-                ></MyAssignmentCard>)
+                currentUserData?.email ?
+
+                    <div className="max-w-7xl mx-auto my-14 grid grid-cols-3 gap-6">
+                    {
+                        currentUserData.map(currentUsers => <MyAssignmentCard
+                            key={currentUsers._id}
+                            currentUsers={currentUsers}
+                            currentUserData={currentUserData}
+                            setCurrentUserData={setCurrentUserData}
+                        ></MyAssignmentCard>)
+                    }
+                    </div>
+                    :
+                    <div className="text-center mx-auto max-w-xl my-10">
+                        <p className="text-3xl font-medium my-10">You have not created any assignment</p>
+                        <img src={img} className="shadow-xl rounded-full" alt="" />
+                    </div>
             }
         </div>
     );
--- src/components/Routes/PrivateRoute.jsx
+++ src/components/Routes/PrivateRoute.jsx
@@ -11,7 +11,11 @@ const PrivateRoute = ({ children }) => {
 
     if (loading) {
         return <div className=" flex justify-center items-center">
-            <span className="loading loading-dots loading-lg text-pink-500"></span>
+            {/* <span className="loading loading-dots loading-lg text-pink-500"></span> */}
+            <span className="loading loading-ball loading-xs"></span>
+            <span className="loading loading-ball loading-sm"></span>
+            <span className="loading loading-ball loading-md"></span>
+            <span className="loading loading-ball loading-lg"></span>
         </div>
     }
 
