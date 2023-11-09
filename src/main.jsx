987e94ec34e793d4481628a01b321c759b143ff1
almuktadiir <muktadir.j729.com>
2023-11-09 18:58:16 +0600

    framer motion added

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,27 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './components/Routes/Router.jsx'
 import AuthProvider from './components/AuthProvider/AuthProvider.jsx'
+import { motion } from "framer-motion"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
-    <AuthProvider>
-    <RouterProvider router={router}></RouterProvider>
+    <motion.div
+      initial={{ scale: 0 }}
+      animate={{ rotate: 360, scale: 1 }}
+      transition={{
+        type: "spring",
+        stiffness: 50,
+        damping: 20
+      }} > 
+      <AuthProvider>
+      <RouterProvider router={router}>
+
+      </RouterProvider>
     </AuthProvider>
+      </motion.div>
+    
   </React.StrictMode>,
 )
+
+
